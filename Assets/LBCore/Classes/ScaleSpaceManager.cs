5a2e0a886dfb2c62e48bc77bee2683e004445150
Grady Lorenzo <gradylorenzo@gmail.com>
2019-12-25 09:27:42 -0500

    Commit before replacing SSM with FOM

---
+++ Assets/LBCore/Classes/ScaleSpaceManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScaleSpaceManager
+{
+    public delegate void UpdateSSOffset(Vector3 offset);
+    public static UpdateSSOffset UpdateScaleSpaceOffset;
+}
